55c53f79efa07c54883fc22d2ab4484efde63b58
husamahmud <husamahmud@gmail.com>
2024-07-02 13:27:58 +0300

    enhancement(front): Remove unnecessary componnets

--- client/src/features/cabins/TableRow.tsx
+++ client/src/features/cabins/TableRow.tsx
@@ -12,11 +12,23 @@ const TableRow: React.FC<{ cabin: CabinI; key: string }> = ({ cabin }) => {
     <div
       role="row"
       className="grid grid-cols-[0.6fr_1.8fr_2.2fr_1fr_1fr_1fr] items-center gap-x-6 px-9 py-5 text-2xl">
-      <Img image={image} />
-      <Cabin cabinNumber={cabinNumber} />
-      <Guests maxCapacity={maxCapacity} />
-      <Price regularPrice={regularPrice} />
-      {discount ? <Discount discount={discount} /> : <span>&mdash;</span>}
+      <img
+        src={image}
+        alt="cabin image"
+        className="block aspect-[3/2] w-28 object-cover object-center"
+      />
+      <div className="font-semibold text-color-grey-600">{cabinNumber}</div>
+      <div>
+        Fits up to <span className="font-semibold">{maxCapacity}</span> guests
+      </div>
+      <div className="font-semibold">{formatCurrency(regularPrice)}</div>
+      {discount ? (
+        <div className="font-semibold text-green-700">
+          {formatCurrency(discount)}
+        </div>
+      ) : (
+        <span>&mdash;</span>
+      )}
 
       <div className="flex gap-3">
         <Button variant="secondary">
@@ -31,35 +43,3 @@ const TableRow: React.FC<{ cabin: CabinI; key: string }> = ({ cabin }) => {
 }
 
 export default TableRow
-
-const Img: React.FC<{ image: string }> = ({ image }) => {
-  return (
-    <img
-      src={image}
-      alt="cabin image"
-      className="block aspect-[3/2] w-28 object-cover object-center"
-    />
-  )
-}
-
-const Cabin: React.FC<{ cabinNumber: number }> = ({ cabinNumber }) => {
-  return <div className="font-semibold text-color-grey-600">{cabinNumber}</div>
-}
-
-const Guests: React.FC<{ maxCapacity: number }> = ({ maxCapacity }) => {
-  return (
-    <div>
-      Fits up to <span className="font-semibold">{maxCapacity}</span> guests
-    </div>
-  )
-}
-
-const Price: React.FC<{ regularPrice: number }> = ({ regularPrice }) => {
-  return <div className="font-semibold">{formatCurrency(regularPrice)}</div>
-}
-
-const Discount: React.FC<{ discount: number }> = ({ discount }) => {
-  return (
-    <div className="font-semibold text-green-700">{formatCurrency(discount)}</div>
-  )
-}
