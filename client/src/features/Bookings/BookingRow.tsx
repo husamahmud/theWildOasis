4895068f47f7231a27c019de1b787fc476a45553
husamahmud <husamahmud@gmail.com>
2024-07-08 14:43:20 +0300

    style(front): Update bookingRow colors

--- client/src/features/Bookings/BookingRow.tsx
+++ client/src/features/Bookings/BookingRow.tsx
@@ -44,7 +44,12 @@ interface BookingRowProps {
 
 const BookingRow: React.FC<BookingRowProps> = ({ booking, cabin, guest }) => {
   const { startDate, endDate, numNight, totalPrice, status } = booking
-  const statusColor = 'green'
+
+  const statusColor = {
+    UNCONFIRMED: `bg-blue-100 text-blue-700`,
+    CHECKED_IN: `bg-green-100 text-green-700`,
+    CHECKED_OUT: `bg-gray-100 text-gray-700`,
+  }
 
   return (
     <div className="grid grid-cols-[0.6fr_1.5fr_2fr_1.4fr_1fr_0.5fr] items-center px-9 py-5">
@@ -72,7 +77,7 @@ const BookingRow: React.FC<BookingRowProps> = ({ booking, cabin, guest }) => {
       </div>
 
       <div
-        className={`w-fit rounded-full uppercase bg-${statusColor}-300 textgre px-4 py-2 text-lg font-semibold`}
+        className={`textgre w-fit rounded-full px-4 py-2 text-lg font-semibold uppercase ${statusColor[status]}`}
       >
         {status.replace('_', ' ')}
       </div>
